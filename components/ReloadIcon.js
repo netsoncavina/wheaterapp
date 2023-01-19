b6197b153c7b17553077be98c5689b49bed35c63
Netson Cavina <netsoncavina@gmail.com>
2023-01-19 15:22:21 -0300

    mudanças de estilo

--- components/ReloadIcon.js
+++ components/ReloadIcon.js
@@ -1,11 +1,24 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import { Ionicons } from "@expo/vector-icons";
 
 export default function ReloadIcon() {
   return (
     <View>
-      <Ionicons name="reload" size={24} color="black" />{" "}
+      <Ionicons
+        style={styles.reloadIcon}
+        name="reload"
+        size={24}
+        color="black"
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  reloadIcon: {
+    position: "absolute",
+    top: 40,
+    right: 20,
+  },
+});
