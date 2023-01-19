742cd62793982b825fdc27109e0e27dd1f1ea232
Netson Cavina <netsoncavina@gmail.com>
2023-01-19 14:51:17 -0300

    mudanças no estilo do componente

--- components/UnitsPicker.js
+++ components/UnitsPicker.js
@@ -1,14 +1,24 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import { Picker } from "@react-native-picker/picker";
 
 export default function UnitsPicker() {
   return (
     <View>
-      <Picker>
+      <Picker style={styles.picker}>
         <Picker.Item label="C°" value="metric" />
         <Picker.Item label="F°" value="imperial" />
       </Picker>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  picker: {
+    position: "absolute",
+    top: 200,
+    left: 10,
+    height: 50,
+    width: 100,
+  },
+});
