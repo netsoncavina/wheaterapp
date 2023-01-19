a312622bd8493e33ff8c12263558b392cb03d18c
Netson Cavina <netsoncavina@gmail.com>
2023-01-19 15:19:13 -0300

    desenvolvimento do componente ReloadIcon

---
+++ components/ReloadIcon.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function ReloadIcon() {
+  return (
+    <View>
+      <Ionicons name="reload" size={24} color="black" />{" "}
+    </View>
+  );
+}
