9e8aa5f507e2375380ab12c37689c8d2885e4a86
Netson Cavina <netsoncavina@gmail.com>
2023-01-24 17:57:17 -0300

    refatoração no código

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ import WeatherInfo from "./components/WeatherInfo";
 import UnitsPicker from "./components/UnitsPicker";
 import ReloadIcon from "./components/ReloadIcon";
 import WeatherDetails from "./components/WeatherDetails";
+import InputText from "./components/InputText";
 import { colors } from "./utils/index";
 
 const WEATHER_API_KEY = "591ed98225dfbeadd70c189a6eb67766";
@@ -21,6 +22,9 @@ export default function App() {
   const [errorMessage, setErrorMessage] = useState(null);
   const [currentWeather, setCurrentWeather] = useState(null);
   const [unitSystem, setUnitSystem] = useState("metric");
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+
   useEffect(() => {
     load();
   }, [unitSystem]);
@@ -36,7 +40,9 @@ export default function App() {
       }
 
       const location = await Location.getCurrentPositionAsync({});
-      const { latitude, longitude } = location.coords;
+      setLatitude(location.coords.latitude);
+      setLongitude(location.coords.longitude);
+
       const weatherUrl = `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitSystem}&lang=pt_br&appid=${WEATHER_API_KEY}`;
 
       const response = await fetch(weatherUrl);
@@ -57,6 +63,7 @@ export default function App() {
       <StatusBar style="auto" />
       <UnitsPicker unitSystem={unitSystem} setUnitSystem={setUnitSystem} />
       <ReloadIcon load={load} />
+      <InputText />
       <View style={styles.main}>
         {currentWeather ? (
           <WeatherInfo currentWeather={currentWeather} />
@@ -91,5 +98,6 @@ const styles = StyleSheet.create({
   main: {
     flex: 1,
     justifyContent: "center",
+    // marginTop: 10,
   },
 });
