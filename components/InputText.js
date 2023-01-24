4ded2c71e824bbda5cc14fc0771400c867feec4f
Netson Cavina <netsoncavina@gmail.com>
2023-01-24 18:28:01 -0300

    adição da função para pesquisar localização

--- components/InputText.js
+++ components/InputText.js
@@ -1,10 +1,26 @@
 import { View, Text, TextInput, StyleSheet } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 
-export default function InputText() {
+export default function InputText({ setLatitude, setLongitude, onSubmit }) {
+  const [text, onTextChange] = useState("");
+
+  async function getCoordinates() {
+    const searchUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${text}&appid=591ed98225dfbeadd70c189a6eb67766`;
+    const response = await fetch(searchUrl);
+    const result = await response.json();
+    setLatitude(result[0].lat);
+    setLongitude(result[0].lon);
+    onSubmit();
+  }
+
   return (
     <View style={styles.container}>
-      <TextInput style={styles.input} placeholder="Insira localização" />
+      <TextInput
+        style={styles.input}
+        placeholder="Insira localização"
+        onSubmitEditing={() => getCoordinates()}
+        onChangeText={(text) => onTextChange(text)}
+      />
     </View>
   );
 }
